e6642f7ce560f2cbcae6c7497fc35dcaf4bde1f1
leok si <leoksi.koh@gmail.com>
2016-06-04 16:38:45 +0800

    add source of api to price

--- app.js
+++ app.js
@@ -22,29 +22,39 @@ app.get('/', function(req, res){
   request.get("https://api.myjson.com/bins/1c80k", function (err, response, body) {
     hotelsData = JSON.parse(body);
     request.get("https://api.myjson.com/bins/2tlb8", function (err, response, body) {
-      price1Data = JSON.parse(body);
+      price1Data = {
+        name: '2tlb8',
+        data: JSON.parse(body)
+      };
       request.get("https://api.myjson.com/bins/42lok", function (err, response, body) {
-        price2Data = JSON.parse(body);
+        price2Data = {
+          name: '42lok',
+          data: JSON.parse(body)
+        };
         request.get("https://api.myjson.com/bins/15ktg", function (err, response, body) {
-          price3Data = JSON.parse(body);
+          price3Data = {
+            name: '15ktg',
+            data: JSON.parse(body)
+          };
 
           var priceDatas = [price1Data, price2Data, price3Data];
 
           for (var i = 0; i < hotelsData.hotels.length; i++){
             var hotelId = hotelsData.hotels[i].id;
             var minPrice = null;
+            var minPriceSource = null;
 
             for (var j = 0; j< priceDatas.length; j++){
-              var price = priceDatas[j][hotelId];
+              var price = priceDatas[j].data[hotelId];
               if (price) {
-                if (minPrice === null){
+                if (minPrice === null || price < minPrice) {
                   minPrice = price;
-                } else if (minPrice > price) {
-                  minPrice = price;
+                  minPriceSource = priceDatas[j].name;
                 }
               }
             }
             hotelsData.hotels[i].minPrice = minPrice;
+            hotelsData.hotels[i].minPriceSource = minPriceSource;
           }
           res.render('index', {
             hotels: hotelsData.hotels
